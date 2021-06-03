7633e72a30de3752a670bfa4b9d8ee9dda7e898e
Lrd134 <larryc3200@gmail.com>
2021-06-03 18:52:43 -0400

    added sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < Sinatra::Base
-  set :views, proc { File.join(root, '../views/') }
-  
+  configure do
+    set :views, "app/views"
+    enable :sessions
+    set :session_secret, "password_security"
+  end
   get '/' do
     erb :index
   end
