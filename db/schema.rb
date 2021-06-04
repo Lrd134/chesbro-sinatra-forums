1340f8ce6ea4465ea21108bbf793e9004af0434e
Lrd134 <larryc3200@gmail.com>
2021-06-04 14:01:58 -0400

    added title to post

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210603232702) do
+ActiveRecord::Schema.define(version: 20210604180056) do
 
   create_table "posts", force: :cascade do |t|
     t.string  "content"
     t.integer "user_id"
+    t.string  "title"
   end
 
   create_table "users", force: :cascade do |t|
