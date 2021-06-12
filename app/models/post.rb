bf3eba64393ac1c617d67dc7f3af38ef7cfff6c8
Lrd134 <larryc3200@gmail.com>
2021-06-12 09:21:36 -0400

    styled users/index

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,11 @@
 class Post < ActiveRecord::Base
   belongs_to :user
-  validates :title, presence: true
-  validates :content, presence: true
+  validates :title, presence: { message: "%{attribute} must be given please." }, uniqueness: {
+    # object = person object being validated
+    # data = { model: "Person", attribute: "Username", value: <username> }
+    message: ->(object, data) do
+      "Hey, #{data[:value]} is already in use as a title."
+    end
+  }
+  validates :content, presence: { message: "%{attribute} must be given please." }
 end
